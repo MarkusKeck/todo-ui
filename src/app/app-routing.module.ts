376721cfe4ffde31dbc7beb1ab45bf5cc7523b3d
MarkusKeck <markus@keck.bayern>
2022-09-22 14:47:26 +0200

    Added components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {TodoListComponent} from "./todo-list/todo-list.component";
+import {LoginComponent} from "./login/login.component";
+import {ImprintComponent} from "./imprint/imprint.component";
+import {PrivacyComponent} from "./privacy/privacy.component";
 
+const routes: Routes = [
+  { path: 'todo-list', component: TodoListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'privacy', component: PrivacyComponent },
+];
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { TodoComponent } from './todo/todo.component';
 import {RouterModule} from "@angular/router";
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { LoginComponent } from './login/login.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
-    TodoListComponent
+    TodoListComponent,
+    LoginComponent,
+    ImprintComponent,
+    PrivacyComponent
   ],
   imports: [
     BrowserModule,
